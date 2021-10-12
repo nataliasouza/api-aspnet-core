a7018b5e2b010b53a89a52c5287ec031ff2df2fb
Natália Souza <nataliasouza.ti@hotmail.com>
2021-10-11 22:46:31 -0300

    ProjetoInicial

---
+++ WebAPI/WebAPI/Models/Categoria.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class Categoria
+    {
+        public Categoria()
+        {
+            Produtos = new Collection<Produto>();
+        }
+        public int CategoriaId { get; set; }
+        public string Nome { get; set; }
+        public string ImagemUrl { get; set; }
+        public ICollection<Produto> Produtos { get; set; }
+    }
+}
