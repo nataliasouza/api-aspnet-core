39c134900c9a200b08f40e96049573ca4bcb4df0
Natália Souza <nataliasouza.ti@hotmail.com>
2021-10-12 22:19:53 -0300

    Crud ProdutosController

--- WebAPI/WebAPI/Controllers/ProdutosController.cs
+++ WebAPI/WebAPI/Controllers/ProdutosController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,10 +18,56 @@ namespace WebAPI.Controllers
         {
             _context = contexto;
         }
+        [HttpPost]
+        public ActionResult BuscarProduto([FromBody]Produto produto)
+        {
+            _context.Produtos.Add(produto);
+            _context.SaveChanges();
+            return new CreatedAtRouteResult("ObterProduto", new { id = produto.ProdutoId }, produto);
+        }
+
         [HttpGet]
         public ActionResult<IEnumerable<Produto>> Get()
         {
-            return _context.Produtos.ToList();
+            return _context.Produtos.AsNoTracking().ToList();
         }
+
+        [HttpGet("{id}", Name ="ObterProduto")]
+        public ActionResult<Produto> BuscarProdutoPorId(int id)
+        {
+            var produto = _context.Produtos.AsNoTracking().FirstOrDefault(p => p.ProdutoId == id);
+            if(produto == null)
+            {
+                return NotFound();
+            }
+            return produto;
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult AlterarProduto(int id, [FromBody]Produto produto)
+        {
+            if( id!= produto.ProdutoId)
+            {
+                return BadRequest();
+            }
+            _context.Entry(produto).State = EntityState.Modified;
+            _context.SaveChanges();
+            return Ok();
+        }
+        [HttpDelete("{id}")]
+        public ActionResult<Produto> DeletarProduto(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+
+            if(produto == null)
+            {
+                return NotFound();
+            }
+
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges();
+            return produto;
+        }
+
     }
 }
