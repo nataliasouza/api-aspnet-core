7d7cd0f94f6df75d8670579e40b0b020a7a7e9c5
Natália Souza <nataliasouza.ti@hotmail.com>
2021-10-12 21:37:03 -0300

    Criei controller de Produtos

---
+++ WebAPI/WebAPI/Controllers/ProdutosController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Context;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public ProdutosController(AppDbContext contexto)
+        {
+            _context = contexto;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            return _context.Produtos.ToList();
+        }
+    }
+}
